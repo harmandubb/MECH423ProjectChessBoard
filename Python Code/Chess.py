5fa4d30964943f1219058a5c7ca951e5fe517cf4
harmandubb <harmand1999@gmail.com>
2022-05-18 18:21:43 -0700

    Camera calbiration done

--- Python Code/Chess.py
+++ Python Code/Chess.py
@@ -1,10 +1,71 @@
 import numpy as np 
 import cv2 as cv 
+import glob
+
+def cameraCalibration():
+    frameSize = (1920,1080)
+    chessboardSize = (7,7)
+    sidelength = 44 # mm 
+    # termination criteria
+    criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((chessboardSize[0]*chessboardSize[1],3), np.float32)
+    objp[:,:2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1,2)*sidelength #44 mm is the sidelength of squares
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+    images = glob.glob('Calibration_Images/*.jpg')
+
+    for fname in images:
+        img = cv.imread(fname)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        # Find the chess board corners
+        ret, corners = cv.findChessboardCornersSB(gray, chessboardSize, None)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            corners2 = cv.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+            imgpoints.append(corners)
+            # Draw and display the corners
+            cv.drawChessboardCorners(img, chessboardSize, corners2, ret)
+            #cv.imshow('img', img)
+            #imS = cv.resize(img, (960, 540))  
+            cv.imshow('img', img)
+            cv.waitKey(10)
+
+    ret, mtx, dist, rvecs, tvecs = cv.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+
+    np.savez("Camera parameters", mtx=mtx,dist=dist)
+    npzfile = np.load("Camera parameters.npz")
+    print(npzfile.files)
+    print(npzfile['mtx'])
+
+    #print(mtx)
+    #print(dist)
+
+    img = cv.imread(images[1])
+    h,  w = img.shape[:2]
+    newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+    # undistort
+    dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+
+
+    # crop the image
+    x, y, w, h = roi
+    dst = dst[y:y+h, x:x+w]
+    cv.imwrite('calibresult.png', dst)
+
+    return mtx, dist
+
+
 
 cap = cv.VideoCapture(1)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
+    
+mtx, dist = cameraCalibration()
 
 while True:
     # Capture frame-by-frame
@@ -25,3 +86,6 @@ while True:
 # When everything done, release the capture
 cap.release()
 cv.destroyAllWindows()
+
+
+    
